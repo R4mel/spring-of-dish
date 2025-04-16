7ee6f960b790e140b42f7d0e60de2721e864b3b9
Ramel <dev_20@naver.com>
2025-04-16 22:33:49 +0900

    [Refactor: back] DATABASE_URL modify

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 데이터베이스 URL 정의
-DATABASE_URL = "mysql+pymysql://root:carpediem12@localhost/spring_of_dish"
+DATABASE_URL = "mysql+pymysql://root:root@localhost/spring_of_dish"
 
 # 엔진 생성
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
