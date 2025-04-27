ac1a9df14560abadc1fd2f9676ead3a23f0b89bd
Ramel <dev_20@naver.com>
2025-04-27 17:31:36 +0900

    [Feat: back] get_db func add

--- backend/database.py
+++ backend/database.py
@@ -6,10 +6,18 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "mysql+pymysql://root:root@localhost/spring_of_dish"
 
 # 엔진 생성
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(DATABASE_URL)
 
 # 세션 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base 클래스 생성
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
