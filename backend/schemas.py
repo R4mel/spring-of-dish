d6853bce9f416a88d1686772db26a61143de17a6
Ramel <dev_20@naver.com>
2025-04-27 17:30:51 +0900

    [Fix: back] UserBase modify

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 class UserBase(BaseModel):
+    nickname: str = Field(..., max_length=100)
     kakao_id: int
     name: str = Field(..., max_length=255)
     email: EmailStr
@@ -13,18 +14,19 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    kakao_id: int  # 필수 필드
+    pass
 
 
 class UserResponse(UserBase):
-    id: int
+    user_no: int
+    register_date: datetime
 
     class Config:
-        orm_mode = True  # SQLAlchemy 모델을 기반으로 Pydantic를 사용하기 위해 설정
+        orm_mode = True
 
 
 class IngredientBase(BaseModel):
-    name: str = Field(..., max_length=255)  # 필수 필드
+    name: str = Field(..., max_length=255)
     added_date: datetime
     limit_date: datetime
 
@@ -41,15 +43,15 @@ class IngredientResponse(IngredientBase):
 
 
 class RecipeBase(BaseModel):
-    title: str = Field(..., max_length=255)  # 필수 필드
-    cooking_method: str = Field(..., max_length=1024)  # 기본값은 없음
+    title: str = Field(..., max_length=255)
+    cooking_method: str = Field(..., max_length=255)
     youtube_video_id: str = Field(..., max_length=255)
-    youtube_url: HttpUrl
-    youtube_thumbnail_url: HttpUrl
+    youtube_url: str
+    youtube_thumbnail_url: str
 
 
 class RecipeCreate(RecipeBase):
-    ingredient_ids: List[int]  # 새로운 레시피를 생성할 때 포함된 재료들의 ID
+    ingredient_ids: List[int]
 
 
 class RecipeResponse(RecipeBase):
@@ -61,9 +63,9 @@ class RecipeResponse(RecipeBase):
 
 
 class StarBase(BaseModel):
-    user_id: int
+    user_no: int
     recipe_id: int
-    count: int = Field(0, ge=0)  # 기본값: 0, 최소값: 0
+    count: int = Field(0, ge=0)
 
 
 class StarCreate(StarBase):
